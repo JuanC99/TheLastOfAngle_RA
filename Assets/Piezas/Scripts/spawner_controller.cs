5b53d82b2eda11f9e8026cd8c7bb398f9d708171
Sergi <sergisise@gmail.com>
2022-11-15 19:36:27 +0100

    Implementacion piezas sprint 2b

---
+++ Assets/Piezas/Scripts/spawner_controller.cs
@@ -0,0 +1,93 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class spawner_controller : MonoBehaviour
+{
+
+    public GameObject pieza;
+    private List<Vector3> listaPosicionesGeneradas;
+    [SerializeField]
+    int numeroDePiezasAGenerar = 10;
+    [SerializeField]
+    float distanciaMinimaAlVehiculo = 5;
+    [SerializeField]
+    float distanciaMinimaEntrePiezas = 5;
+    [SerializeField]
+    GameObject vehiculo;
+    [SerializeField]
+    Vector3 posInicialVehiculo;
+    [SerializeField]
+    float radio = 10;
+    [SerializeField]
+    GameObject terreno;
+    float bordeX = 0f;
+    float bordeZ = 0f;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //obtenemos la posicion inicial del vehiculo
+        posInicialVehiculo = vehiculo.transform.position;
+        //obtenemos los bordes
+        MeshRenderer mr = terreno.GetComponent<MeshRenderer>();
+        bordeX = mr.bounds.size.x;
+        bordeZ = mr.bounds.size.z;
+        //Debug.Log("Borde X : " + bordeX);
+        //Debug.Log("Borde Z : " + bordeZ);
+
+        //instanciamos la lista que guardará las posiciones donde hay objetos
+        listaPosicionesGeneradas = new List<Vector3>();
+
+        //llamamos al metodo para generar las piezas
+        generarPiezas(numeroDePiezasAGenerar, distanciaMinimaAlVehiculo, distanciaMinimaEntrePiezas, mr.bounds.center, radio);
+    }
+
+    public void generarPiezas(int numeroAGenerar,float distanciaMinimaAlVehiculo,float distanciaMinimaEntrePiezas,Vector3 origen,float radioMaximo)
+    {
+        int contadorPiezasGeneradas = 0;
+        while(contadorPiezasGeneradas < numeroAGenerar)//comprobamos si ya se han generado todas
+        {
+            Vector3 randomPosition = origen + Random.insideUnitSphere * radioMaximo;//creamos una coordenada random
+            randomPosition.y = 0;
+            
+            
+            
+                if(contadorPiezasGeneradas != 0)//comprobamos que no sea la primera
+                {
+                    bool estaLejosDeOtrasPiezas = true;
+                    foreach (Vector3 pos in listaPosicionesGeneradas)//recorremos la lista de posiciones
+                    {
+                        if(Vector3.Distance(pos,randomPosition) < distanciaMinimaEntrePiezas)//si no esta a la distancia minima de otras piezas no se genera
+                        {
+                            estaLejosDeOtrasPiezas = false;
+                        }
+                    }
+                    if (estaLejosDeOtrasPiezas && Vector3.Distance(posInicialVehiculo, randomPosition) > distanciaMinimaAlVehiculo && Mathf.Abs(randomPosition.x) < bordeX && Mathf.Abs(randomPosition.z) < bordeZ)
+                    {
+                    //si pasa todas las comprobaciones se añade
+                        Instantiate(pieza, randomPosition, Quaternion.identity);
+                        contadorPiezasGeneradas++;
+                        listaPosicionesGeneradas.Add(randomPosition);
+                    }
+                    
+                }
+                else
+                {
+                    //si es la primera
+                    //comprobamos si esta a la distancia minima del vehiculo y si esta dentro del plano
+                    if(Vector3.Distance(posInicialVehiculo,randomPosition) > distanciaMinimaAlVehiculo && Mathf.Abs(randomPosition.x) < bordeX && Mathf.Abs(randomPosition.z) < bordeZ)
+                    {
+                        Instantiate(pieza, randomPosition, Quaternion.identity);
+                        contadorPiezasGeneradas++;
+                        listaPosicionesGeneradas.Add(randomPosition);
+                    }
+                    
+                }
+                
+            
+            
+        }
+        
+    }
+}
